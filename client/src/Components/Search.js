bafea9e1942391fe853f3df9a81dfeb5e02fb262
yash-helix <yash.netankar@helixstack.in>
2022-10-21 12:12:39 +0530

    initial commit

---
+++ client/src/Components/Search.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import UserTable from "./UserTables";
+import "./Search.css";
+
+const Search = () => {
+  const [filterData, setFilterData] = useState("");
+
+  return (
+    <>
+      <div className="header">
+        <h2>Marketing Management</h2>
+      </div>
+
+      <div className="searchBox mt-5 container-fluid">
+        <input
+          className="form-control w-75 mx-auto"
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setFilterData(e.target.value)}
+        />
+        <UserTable filterData={filterData} />
+      </div>
+    </>
+  );
+};
+
+export default Search;
---
+++ client/src/Components/TestingComponent/Testing.js
@@ -0,0 +1,165 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Box, Stack, Card, Button } from '@mui/material';
+
+import Logo from "../Logo/Logo";
+import ChevronRightOutlinedIcon from '@mui/icons-material/ChevronRightOutlined';
+// import QuestionChips from '../QuestionChips/QuestionChips';
+import QuestionTable from '../QuestionTable/QuestionTable';
+import axios from 'axios'
+import { useNavigate } from "react-router";
+
+
+const FinalScreen = () => {
+    const [data, setData] = useState([]);
+    const [country, setCountry] = useState("");
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        window.onbeforeunload = function () {
+            return "Data will be lost if you leave the page, are you sure?";
+        };
+
+        return () => {
+            window.onbeforeunload = null;
+        };
+    }, []);
+
+
+    useEffect(() => {
+        (async () => {
+            const testID = localStorage.getItem("testID");
+            const userID = localStorage.getItem("userID")
+            // const { testID, userID } = user;
+            const data = {
+                testID,
+                userID
+            }
+
+            // =====================================================================================
+
+            const res = await axios.post(`${process.env.REACT_APP_SERVER}/user/getQuestionFromId`, data);
+
+            if (res.data.success) {
+                let questionsArr = res.data.data;
+                questionsArr = questionsArr.map(q => ({ ...q, choice: 0 }))
+                setData(questionsArr);
+            }
+            else {
+                if (res.data.isTestStarted) navigate("/startTest", { replace: true });
+                else if (res.data.retestExhausted) navigate("/retestExhasuted", { replace: true });
+                else {
+                    alert(res.data.msg);
+                    navigate("/startTest", { replace: true });
+                }
+            }
+
+            // =========================================================================================
+        })();
+    }, []);
+
+    useEffect(() => {
+        const getCountry = async() => {
+          // https://api.ipify.org/
+          const res = await axios.get("http://ip-api.com/json");
+          setCountry(res.data.country)
+        }
+        getCountry();
+      }, [])
+
+
+    const [index, setIndex] = useState(0);
+    const setAns = (choice) => {
+        data[index].choice = choice
+    }
+
+    async function Submit() {
+        let finalAnswers = data.map(d => ({ questionID: d._id, answer: d.choice }));
+
+        let userID = localStorage.getItem("userID");
+        const data1 = {
+            userID,
+            questions: finalAnswers,
+            country
+        }
+
+        const res = await axios.post(`${process.env.REACT_APP_SERVER}/user/submitTest`, { data: data1 });
+        if (res.data.success) {
+            navigate("/TestCompleted", { replace: true });
+        }
+        else {
+            alert(res.data.msg);
+            navigate("/startTest", { replace: true });
+        }
+    }
+
+
+
+
+    return (
+        <>
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', }} padding={"1rem"} id='timer'>
+                <Logo />
+
+            </Box>
+            {data[index]?.Images.length > 0 ? (
+                <Stack flexDirection='column' >
+                    <Stack className="testing" sx={{ display: "flex", flexDirection: "row", justifyContent: 'space-between' }}>
+
+
+                        <Card sx={{ flex: 1, boxShadow: 'none' }}>
+
+                            <QuestionTable data={data[index]} index={index} sendAns={setAns} />
+                        </Card>
+
+                        <Card sx={{ flex: 0.8, boxShadow: 'none' }}>
+                            {data[index].Images.map((img, key) => <img
+                                src={"https://luke-pdf-image.s3.ap-south-1.amazonaws.com/" + img} key={key} className="img-fluid" />)}
+                        </Card>
+                    </Stack>
+
+                    <Stack sx={{ justifyContent: 'center', alignItems: 'center', m: 2, mt: 5 }}>
+
+                        {index < data.length - 1 && <Button sx={{
+                            "&:hover": { 'background': 'lightblue' }, color: 'white', backgroundColor: 'green', padding: '0.5rem 1rem',
+                            mt: '5%',
+                        }} onClick={() => setIndex(i => i + 1)} > Submit Answer <ChevronRightOutlinedIcon /> </Button>}
+                        {index === data.length - 1 && <Button variant='contained' onClick={Submit} sx={{ padding: '0.5rem 1rem', }} > Submit Answer </Button>}
+                    </Stack>
+                </Stack>
+            )
+
+                :
+
+                (
+                    <Stack flexDirection='column'>
+
+                        <Stack className="" sx={{ display: "flex", flexDirection: { xs: "column", sm: "row" } }}>
+
+                            <Card sx={{ flex: 1, boxShadow: 'none' }}>
+                                <QuestionTable data={data[index]} index={index} sendAns={setAns} />
+                            </Card>
+
+                        </Stack>
+
+                        <Stack sx={{ justifyContent: 'center', alignItems: 'center', m: 2, mt: 5, }}>
+                            {index < data.length - 1 && <Button sx={{
+                                "&:hover": {
+                                    'background': 'lightblue'
+                                }, color: 'white', backgroundColor: 'green', padding: '0.5rem 1rem',
+
+                            }} onClick={() => setIndex(i => i + 1)}> Submit Answer <ChevronRightOutlinedIcon /> </Button>}
+                            {index === data.length - 1 && <Button variant='outlined' size='medium' sx={{
+                                mt: '5%',
+                                color: 'white', backgroundColor: 'green'
+                            }} onClick={Submit}> Submit Answer </Button>}
+                        </Stack>
+
+                    </Stack>)
+            }
+
+        </>
+    )
+}
+export default FinalScreen;
+
+
